5fce638bafb762f8a7d44efe57fc080617fe2669
abes1809 <amandanicole321@gmail.com>
2018-01-23 21:26:15 -0600

    added happy and sad routes for error

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,8 +13,11 @@ class ProductsController < ApplicationController
                          image_url: params[:image_url],
                          description: params[:description]
                           )
-    product.save 
-    render json: product.as_json
+    if product.save 
+      render json: product.as_json
+    else 
+      render json: {errors: product.errors.full_messages}, status: :unprocessable_entity
+    end 
   end 
 
   def show 
@@ -30,9 +33,12 @@ class ProductsController < ApplicationController
     product.price = params[:price] || product.price
     product.image_url = params[:image_url] || product.image_url
     product.description = params[:description] || product.description
-    product.save
-
-    render json: product.as_json 
+    
+    if product.save
+      render json: product.as_json 
+    else   
+      render json: {errors: product.errors.full_messages}, status: :unprocessable_entity
+    end 
   end 
 
   def destroy 
